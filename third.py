21a95e247dad17e11291a1b56b2545d3920e1888
Valera <valerylarionov06@gmail.com>
2024-02-03 18:00:32 +0300

    лево и право в 3 задании

---
+++ third.py
@@ -0,0 +1,96 @@
+import math
+import os
+import sys
+
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit
+from PyQt5.QtCore import Qt
+
+SCREEN_SIZE = [600, 450]
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def get_image(self, coords, scale):
+        if scale > 21:
+            scale = 21
+        elif scale <= 0:
+            scale = 1
+        scale = int(scale)
+
+        if scale == 1 or scale == 0:  # более красивое отображение
+            coords = coords.split(',')
+            coords[-1] = '0'
+            coords = ','.join(coords)
+
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords}&z={scale}&l=map"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            quit()
+
+        # Запишем полученное изображение в файл.
+        self.map_file = f"map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def initUI(self):
+        self.coords = "39.847061,57.576481"
+        self.scale = 1  # значения от 1 до 21
+
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Задание 3')
+        self.get_image(self.coords, self.scale)
+
+        ## Изображение
+        self.pixmap = QPixmap('map.png')
+        self.image = QLabel(self)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp and self.scale < 21:
+            self.scale += 1
+        elif event.key() == Qt.Key_PageDown and self.scale > 0:
+            self.scale -= 1
+
+        if event.key() == Qt.Key_Left:
+            cords = self.coords.split(',')
+            step = 360 / pow(2, self.scale)
+            cords[0] = str(float(cords[0]) - abs(step))
+            if abs(float(cords[0])) >= 180:
+                return
+            self.coords = ','.join(cords)
+
+        elif event.key() == Qt.Key_Right:
+            cords = self.coords.split(',')
+            step = 360 / pow(2, self.scale)
+            cords[0] = str(float(cords[0]) + abs(step))
+            if abs(float(cords[0])) >= 180:
+                return
+            self.coords = ','.join(cords)
+        self.get_image(self.coords, self.scale)
+        self.image.setPixmap(QPixmap('map.png'))
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove('map.png')
+
+
+def except_hook(cls, exception, traceback):
+    sys.excepthook(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    sys.excepthook = except_hook
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
